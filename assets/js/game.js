7282498bf3a8ba3866410cde7ecf43764f93bcdf
mmilner29 <madisonm4514@gmail.com>
2022-02-05 12:54:31 -0700

    Adding & Storing Robot Name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.group("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// this creates a funciton named "fight"
+
+function fight() {
+window.alert("The fight has begun!");
+}
+
+// fight();
